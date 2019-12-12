dc2518e916b6bb009b92618ddad2f8293a1d348d
Zhenyuan <zhenyuan.li@outlook.com>
2019-12-12 22:41:40 +0100

    Replace dbcontext and entities to project ef

---
+++ CleanCompleteArchitecture.Data.EF/Models/Customers1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CleanCompleteArchitecture.Data.EF.Models
+{
+    public partial class Customers1
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerCategoryName { get; set; }
+        public string PrimaryContact { get; set; }
+        public string AlternateContact { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FaxNumber { get; set; }
+        public string BuyingGroupName { get; set; }
+        public string WebsiteUrl { get; set; }
+        public string DeliveryMethod { get; set; }
+        public string CityName { get; set; }
+        public string DeliveryRun { get; set; }
+        public string RunPosition { get; set; }
+    }
+}
